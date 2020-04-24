333f4894bef64a5dcb7e4832c3f1419e101c61cf
Alfred <AlfredArvidsson@Protonmail.com>
2020-04-24 14:07:02 +0200

    added insert function and removed auto-increment on ID column

--- pizzakit-plugin/includes/pizzakit.php
+++ pizzakit-plugin/includes/pizzakit.php
@@ -27,15 +27,41 @@ class Pizzakit {
 		// form so that we don't respond to other post requests.
 		// In this the example it's "pizzakitFormSubmission".
 		if (isset($data["pizzakitFormSubmission"])) {
-			// $data is an associative array with the JSON data do something
-			// nice with it here.
+
+			// insert the data into the different tables
+			// This assumes that the data is in a map-structure with the column names as keys
+			insert_into_tables($data);
 
 			// Respond with a JSON object by calling
 			// wp_send_json($response);
 			// where $response is an associative array.
 	}
 
-	
+
+	private static function insert_into_tables($_data){
+
+		//insert into orders
+		$sql = "INSERT INTO wp_orders(email, name, telNr, address, doorCode, postalCode)
+			VALUES ("
+				. $_data['email'] . ", "
+				. $_data['name'] . ", "
+				. $_data['telNr'] . ", "
+				. $_data['address'] . ", "
+				. $_data['doorCode'] . ", "
+				. $_data['postalCode'] . ",)";
+
+		$wpdb->query($sql);
+
+		//insert into entries
+		$sql = "INSERT INTO wp_entries(orderID, item, quantity)
+			VALUES (
+				(SELECT MAX(id)+1 FROM wp_orders)), "
+				. $_data['item'] . ", "
+				. $_data['quantity'] . ")";
+
+		$wpdb->query($sql);
+
+	}
 }
 
 ?>
