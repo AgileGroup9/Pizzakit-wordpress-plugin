da4653c14aa43f69bcb5e81ca77af4e1673d710b
Alfred <AlfredArvidsson@Protonmail.com>
2020-04-25 13:39:25 +0200

    working inserts function that correctly handles post request with info inside and distributes it throughout the databases tables

--- pizzakit-plugin/includes/pizzakit.php
+++ pizzakit-plugin/includes/pizzakit.php
@@ -22,18 +22,13 @@ class Pizzakit {
 	private static function handle_post() {
 
 		$json = file_get_contents("php://input");
-		//$json = file_get_contents("../wp-content/plugins/pizza-plugin/testjson.txt");
-		//file_put_contents("../wp-content/plugins/pizza-plugin/contents.txt", $json);
 		$data = json_decode($json, true);
-		//file_put_contents("../wp-content/plugins/pizza-plugin/contents2.txt", $data);
-
 
 		// Create a "secret" JSON field that's only set when posting from our
 		// form so that we don't respond to other post requests.
 		// In this the example it's "pizzakitFormSubmission".
 		if (isset($data["pizzakitFormSubmission"])) {
 
-			// ------ "Critical error" is caused at this line.
 			Pizzakit::insert_into_tables($data);
 
 			// Respond with a JSON object by calling
@@ -44,31 +39,34 @@ class Pizzakit {
 
 	private static function insert_into_tables($_data){
 
+		global $wpdb;
+
+		// Get the ID to use for this order
+		$_result = $wpdb->get_results("SELECT MAX(id)+1 AS maxID FROM wp_orders", OBJECT);
+		$_orderID	= $_result[0]->maxID;
+
 		//insert into orders
-		$sql = 'INSERT INTO wp_orders(email, name, telNr, address, doorCode, postalCode) VALUES ('
-				. $_data["email"] . ', '
-				. $_data["name"] . ', '
-				. $_data["telNr"] . ', '
-				. $_data["address"] . ', '
-				. $_data["doorCode"] . ', '
-				. $_data["postalCode"] . ', '
-				. $_data["comments"] . ')';
+		$sql = "INSERT INTO wp_orders(id, email, name, telNr, address, doorCode, postalCode, comments) VALUES ("
+				. $_orderID . ", '"
+				. $_data["email"] . "', '"
+				. $_data["name"] . "', '"
+				. $_data["telNr"] . "', '"
+				. $_data["address"] . "', '"
+				. $_data["doorCode"] . "', '"
+				. $_data["postalCode"] . "', '"
+				. $_data["comments"] . "')";
 		$wpdb->query($sql);
 
-		// ------ Get the ID to use for this order - might be broken
-		$_orderID = $wpdb->get_results("SELECT MAX(id)+1 FROM wp_orders", OBJECT);
-
 		//insert into entries
-		foreach ($_data['cart'] as $_item){
+		foreach ($_data["cart"] as $_item){
 			$sql = "INSERT INTO wp_entries(orderID, item, quantity)
 				VALUES ("
-					. $_orderID . ", "
-					. $_item[0] . ", "
-					. $_item[1] . ")";
+					. $_orderID . ", '"
+					. $_item[0] . "', '"
+					. $_item[1] . "')";
 
 					$wpdb->query($sql);
 		}
-
 	}
 }
 
