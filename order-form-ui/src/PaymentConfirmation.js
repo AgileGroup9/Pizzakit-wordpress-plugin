a1cd5ddb64dcee113a9b7d943afdae1365fa2034
Mårten Åsberg <marten.asberg@outlook.com>
2020-05-04 21:33:36 +0200

    Basic layout

---
+++ order-form-ui/src/PaymentConfirmation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class PaymentConfirmation extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<p className="payment-confirmation">
+				<figure/>
+				<progress/>
+				<div>
+					<p>Vi har mottagit din order, och väntar nu på din betalning.</p>
+				</div>
+			</p>
+		);
+	}
+}
+
+export default PaymentConfirmation;
