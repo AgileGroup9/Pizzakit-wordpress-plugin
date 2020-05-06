c8c9c15d4dcdc3f4323a1c5eaa3f136b33af205b
unknown <lukas.s.andersson@gmail.com>
2020-05-06 09:32:48 +0200

    Added Confirmation Failed

---
+++ order-form-ui/src/ConFailed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ConFailed extends React.Component {
+	
+	render() {
+		return(
+			<div className="confirm-page">
+				<h2>Ojsann!</h2>
+				<p>Det gick snett med din order så var vänlig försök igen!</p>
+			</div>
+		);
+	}
+}
+
+export default ConFailed;
