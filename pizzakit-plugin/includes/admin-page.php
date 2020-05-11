8c4109fc36b101ac1cd1c547342ccc7bdfc307dd
unknown <theoman96@gmail.com>
2020-05-11 13:59:11 +0200

    Added HTML and CSS for basic menu editor

--- pizzakit-plugin/includes/admin-page.php
+++ pizzakit-plugin/includes/admin-page.php
@@ -45,7 +45,65 @@ if ($_POST["page"] == "edit-menu") {
             </ul>
           </div>
         </nav>';
-  echo "Theos kod";
+        $sql = "SELECT * FROM wp_items";
+        $items = $wpdb->get_results($sql);
+      
+        echo '<ul class="list-group">
+                <li class="list-group-item">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-sm-r col-xs-3">
+                        <h3>Ändra Menyval</h3>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="list-group-item">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-sm-r col-xs-3">
+                        <b>Namn</b>
+                      </div>
+                      <div class="col-sm-r col-xs-3">
+                        <b>Pris</b>
+                      </div>
+                      <div class="col-sm-r col-xs-3">
+                        <b>Kommentar</b>
+                      </div>
+                      <div class="col-sm-r col-xs-3">
+                        <b>Aktiv</b>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ';
+      
+        foreach ($items as $i) {
+          echo 
+              '<li class="list-group-item">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-sm-3 col-xs-3">
+                      ' . $i->name . 
+                    '</div>
+                    <div class="col-sm-3" col-xs-3>
+                      ' . $i->price . 'kr
+                    </div>
+                    <div class="col-sm-3 col-xs-3">
+                      ' . $i->comment . '
+                    </div>
+                    <div class="col-sm-3 col-xs-3">
+                      <label class="switch">
+                        <input type="checkbox">
+                          <span class="slider round">
+                          </span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </li>';   
+        }
+        echo '</ul>';
 }
 
 elseif ($_POST["page"] == "all_orders") {
@@ -278,3 +336,70 @@ else {
   }
 }
 ?>
+?>
+
+<style>
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 24px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 20px;
+  width: 20px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(34px);
+  -ms-transform: translateX(34px);
+  transform: translateX(34px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+</style>
