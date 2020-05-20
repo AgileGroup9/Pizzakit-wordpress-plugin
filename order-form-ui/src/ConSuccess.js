1cacf6dea694bd49ff82ae01916c9e4fd905e4bc
jakobhenden <62751344+jakobhenden@users.noreply.github.com>
2020-05-20 14:54:44 +0200

    Update ConSuccess.js

--- order-form-ui/src/ConSuccess.js
+++ order-form-ui/src/ConSuccess.js
@@ -6,7 +6,7 @@ class ConSuccess extends React.Component {
 		return(
 			<div className="confirm-page">
 				<h2>Tack!</h2>
-				<p>Betalning är nu mottagen och din order har placerats, leverans kommer att ske innan 16:00 kommande arbetsdag!</p>
+				<p>Betalning är nu mottagen och din order har placerats, du kan hämta din order på fredag!</p>
 				<p>Information om din beställning samt kvitto har skickats ut via mail.</p>
 			</div>
 		);
